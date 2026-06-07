03c28d1ba59c73dd2b241d3658b7cedb90393541
F.Chourli <F.Chourli@DESKTOP-8JUQUAO>
2026-06-07 16:14:30 +0330

    git ignore

--- AllTests/obj/Debug/net8.0/AllTests.AssemblyInfo.cs
+++ AllTests/obj/Debug/net8.0/AllTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AllTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f05dd6af2ca331d12af004f8db4c48d364360b8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AllTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AllTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
